8da6fa82eb9cab314b4130c761eedec657bee49d
WishmithDissanayaka <wishmithdissanayaka@gmail.com>
2024-12-17 21:17:12 +0530

    Modified navbar

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -6,12 +6,13 @@ import { Input } from "@/components/ui/input"
 
 export function SiteHeader() {
     return (
-        <header className="fixed top-0 w-full z-50 bg-white/80 backdrop-blur-md">
+        <header className="fixed top-0 w-full z-50 backdrop-blur-md" >
             <div className="container flex h-16 items-center justify-between px-4">
                 <div className="flex items-center gap-6">
                     <Link href="/" className="flex items-center space-x-2">
                         <img src="/Logo.png" alt="" height={93} width={63}/>
                     </Link>
+                    <br />
                     <div className="hidden lg:flex items-center space-x-1 relative">
                         <Search className="absolute left-3 h-4 w-4 text-muted-foreground" />
                         <Input
@@ -21,7 +22,7 @@ export function SiteHeader() {
                         />
                     </div>
                 </div>
-                <nav className="flex items-center gap-4 lg:gap-6">
+                <nav className="flex items-center gap-4 lg:gap-6 absolute right-3" >
                     <Link
                         href="/"
                         className="text-sm font-medium text-muted-foreground hover:text-primary transition-colors"
